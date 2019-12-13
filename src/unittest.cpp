9fdf0edf3bf51faa7d1ffe7fd134271b2a930497
Yanglin1 <yanglin1@insta360.com>
2019-12-13 17:22:04 +0800

    1.修改 引入unittest模块

--- src/unittest.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "unittest.h"
-
-void UnitTest_Image_GetImageTypeFromFile(char *filename)
-{
-    std::cout << "image type: " << GetImageTypeFromFile(filename) << std::endl;
-}
--- unit_tests/unit_tests.cpp
+++ unit_tests/unit_tests.cpp
@@ -2,11 +2,10 @@
 
 #include "common.h"
 #include "image.h"
-#include "unittest.h"
+
 using namespace std;
 int main(int argc, char *argv[])
 {
-    //UnitTest_Image_GetImageTypeFromFile("yanglin.png");
 	cout << "[Main] Main Function." << endl;
     return 0;
 }
