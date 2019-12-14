0cc5b818bcc5e4e572b0ba89ab64e9c05e174487
Yanglin <185284033@qq.com>
2019-12-15 00:15:26 +0800

    1.增加 编译系统对矩阵库eigen3的支持(ubuntu pc的eigen3安装可以看README) 2.增加 编写unittet，引用eigen3矩阵运算，测试第三方库的成功运用

--- unit_tests/unit_tests.cpp
+++ unit_tests/unit_tests.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-
+#include <Eigen/Dense>
 #include "common.h"
 #include "image.h"
 
 #define CATCH_CONFIG_MAIN          //catch2的main函数
 #include "catch2.h"
-
+using namespace Eigen;
 
 TEST_CASE( "simple" )
 {
     REQUIRE( OPENDIP_IMAGE_PNG == GetImageTypeFromFile((char *)"yanglin.png") );
+}
+
+TEST_CASE("empty")
+{       
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << "Here is the matrix m:\n" << m << std::endl;
+  VectorXd v(2);
+  v(0) = 4;
+  v(1) = v(0) - 1;
+  std::cout << "Here is the vector v:\n" << v << std::endl;
+
+ REQUIRE( 0 == 0 );
 }
